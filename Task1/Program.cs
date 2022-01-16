a4582dcb150372902f432d8c1a00a9b759487902
Аркадий Кунаев <kunayev@mail.ru>
2022-01-16 18:52:37 +0300

    Add project files.

---
+++ Task1/Program.cs
@@ -0,0 +1,28 @@
+﻿//  Выберите любую папку на своем компьютере, имеющую вложенные директории. Выведите на консоль ее содержимое и содержимое всех подкаталогов.
+using System.IO;
+
+
+string path = Directory.GetCurrentDirectory();
+path = Directory.GetParent(path).FullName;
+path = Directory.GetParent(path).FullName;
+path = Directory.GetParent(path).FullName;
+
+getdirs(path);
+
+Console.ReadKey();
+
+
+
+static void getdirs(string path)
+{
+    if (Directory.Exists(path))
+    {
+        foreach (string dir in Directory.GetDirectories(path))
+        {
+            Console.WriteLine(dir);
+            getdirs(dir);
+        }
+        foreach (string file in Directory.GetFiles(path))
+        { Console.WriteLine(file); }
+    }
+}
